c0f44284a3120ec6e49a41a32680935dc62ef7b1
GG\ubb <a@b.com>
2022-10-03 16:46:57 +0900

    ** 강의내용 적용 **

--- products/client/src/router/index.js
+++ products/client/src/router/index.js
@@ -1,7 +1,6 @@
-import {
-  createRouter,
-  createWebHistory
-} from 'vue-router'
+
+import {createRouter,createWebHistory } from 'vue-router'
+
 import ProductList from '../views/ProductList.vue'
 import ProductDetail from '../views/ProductDetail.vue'
 import ProductCreate from '../views/ProductCreate.vue'
@@ -9,37 +8,12 @@ import ProductUpdate from '../views/ProductUpdate.vue'
 import SalesList from '../views/SalesList.vue'
 import ImageInsert from '../views/ImageInsert.vue'
 
-const routes = [{
-    path: '/',
-    name: 'Home',
-    component: ProductList
-  },
-  {
-    path: '/detail',
-    name: 'ProductDetail',
-    component: ProductDetail
-  },
-  {
-    path: '/create',
-    name: 'ProductCreate',
-    component: ProductCreate
-  },
-  {
-    path: '/update',
-    name: 'ProductUpdate',
-    component: ProductUpdate
-  },
-  {
-    path: '/sales',
-    name: 'SalesList',
-    component: SalesList
-  },
-  {
-    path: '/image_insert',
-    name: 'ImageInsert',
-    component: ImageInsert
-  }
-]
+const routes = [{    path: '/'            ,    name: 'Home'          ,    component: ProductList    },
+                {    path: '/detail'      ,    name: 'ProductDetail' ,    component: ProductDetail  },
+                {    path: '/create'      ,    name: 'ProductCreate' ,    component: ProductCreate  },
+                {    path: '/update'      ,    name: 'ProductUpdate' ,    component: ProductUpdate  },
+                {    path: '/sales'       ,    name: 'SalesList'     ,    component: SalesList      },
+                {    path: '/image_insert',    name: 'ImageInsert'   ,    component: ImageInsert    }]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
@@ -47,3 +21,5 @@ const router = createRouter({
 })
 
 export default router
+
+
